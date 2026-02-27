0d68c8b4a5c17dea1a510bf9e89dea060dd95a09
VincentBurke <5258253+VincentBurke@users.noreply.github.com>
2026-02-27 20:54:23 +0000

    chore(cask): update termy cask for v0.1.33

--- Casks/termy.rb
+++ Casks/termy.rb
@@ -1,9 +1,9 @@
 cask "termy" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.1.32"
-  sha256 arm:   "4679f4b812e4aea4e6f15affb48be615a50a06c198bad6938397b6b1dae34d52",
-         intel: "bbce461e1775eb8d4321e1e4a3e7a2cc15950097fb77d3684dd00389196f67b6"
+  version "0.1.33"
+  sha256 arm:   "fd0b8a25f50cef003eed4d454c2b62c14351994060b6f8faf17f6a7375fe2b65",
+         intel: "cb8aecff39d694b782d6f483b21ac56075e7d63fc1aac494ffcab6808d84547c"
 
   url "https://github.com/lassejlv/termy/releases/download/v#{version}/Termy-v#{version}-macos-#{arch}.dmg"
   name "Termy"
